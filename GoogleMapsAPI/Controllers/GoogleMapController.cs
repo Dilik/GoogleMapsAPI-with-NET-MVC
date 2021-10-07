6a21d4c771292595bc160cb6eadf45f88f6d901c
1989deny <1989deny@gmail.com>
2021-10-07 20:34:38 +0500

    Add project files.

---
+++ GoogleMapsAPI/Controllers/GoogleMapController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoogleMapsAPI.Controllers
+{
+    public class GoogleMapController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Details()
+        {
+            return View();
+        }
+    }
+}
